16d2cb15d8f400fd2d805745e50304dbcdf2a56c
Praveen Perumal <ppraveen98841@gmail.com>
2026-02-15 10:22:35 +0000

    Use reactive form instead of form template and perform form validation

--- frontend/src/app/components/transfer/transfer.component.ts
+++ frontend/src/app/components/transfer/transfer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { Account } from '../../models/account.model';
 import { TransferRequest } from '../../models/transfer-request.model';
@@ -13,29 +13,24 @@ import { TransferService } from '../../services/transfer.service';
 @Component({
     selector: 'app-transfer',
     standalone: true,
-    imports: [CommonModule, FormsModule],
+    imports: [ReactiveFormsModule,CommonModule],
     templateUrl: './transfer.component.html',
     styleUrls: ['./transfer.component.css']
 })
 export class TransferComponent implements OnInit {
-    transferRequest: TransferRequest = {
-        fromAccountId: '',
-        toAccountId: '',
-        amount: null!,
-        idempotencyKey: ''
-    };
-
-    account = signal<Account | null>(null);
-
-    successMessage = signal<string>('');
-    errorMessage = signal<string>('');
-    lastTransaction = signal<TransferResponse | null>(null);
-
-    failedTransaction = signal<any | null>(null);
-    showBalance = signal<boolean>(false);
+    transferForm!: FormGroup;
+    submitted = signal<boolean>(false);
     isLoading = signal<boolean>(false);
 
+    transferResponse = signal<TransferResponse | null>(null);
+
+    account = signal<Account | null>(null);
+    showBalance = signal<boolean>(false);
+
+    failedTransaction = signal<any | null>(null);
+
     constructor(
+        private fb: FormBuilder,
         private authService: AuthService,
         private accountService: AccountService,
         private transferService: TransferService
@@ -46,54 +41,58 @@ export class TransferComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.transferForm = this.fb.group({ 
+            toAccountId: ['', [Validators.required, Validators.minLength(14), Validators.maxLength(14)]],
+            amount: [null, [Validators.required, Validators.min(0.01)]]
+         })
+
         if (!this.authService.loggedIn) return;
 
-        this.transferRequest.fromAccountId = this.authService.accountId!;
-        this.transferRequest.idempotencyKey = crypto.randomUUID();
+        // this.transferRequest.fromAccountId = this.authService.accountId!;
+        // this.transferRequest.idempotencyKey = crypto.randomUUID();
 
         this.fetchSenderDetails();
     }
 
     fetchSenderDetails(): void {
-        this.accountService.fetchAccount(this.transferRequest.fromAccountId).subscribe({
+        this.accountService.fetchAccount(this.authService.accountId!).subscribe({
             next: (account) => {
                 this.account.set(account);
-            },
-            error: (error) => {
-                console.error('Error fetching sender details:', error);
-                this.errorMessage.set('Failed to load sender account details.');
             }
         });
     }
 
     onSubmit(): void {
-        if (!this.account()) {
-            this.errorMessage.set('Sender details not loaded cannot proceed');
-            return;
+        // will be set to true
+        this.submitted.set(true);
+
+        // guard statement
+        if (this.transferForm.invalid) return;
+
+        const {toAccountId, amount} = this.transferForm.value;
+        const transferRequest: TransferRequest = {
+            fromAccountId: this.authService.accountId!,
+            toAccountId,
+            amount,
+            idempotencyKey: crypto.randomUUID()
+
         }
 
         this.isLoading.set(true);
-        this.errorMessage.set('');
-        this.successMessage.set('');
-        this.lastTransaction.set(null);
-        this.failedTransaction.set(null);
-
-        this.transferService.transfer(this.transferRequest).subscribe({
+        this.transferService.transfer(transferRequest).subscribe({
             next: (response) => {
-                this.successMessage.set('Transfer successful!');
-                this.lastTransaction.set(response);
+                this.transferResponse.set(response);
                 this.isLoading.set(false);
 
                 // refresh account details to show updated balance
                 this.fetchSenderDetails();
             },
             error: (error) => {
-                console.error('Transfer failed:', error);
                 this.failedTransaction.set({
                     message: error.error?.error || "Transaction failed due to unknown exception. Please contact customer support.",
                     timestamp: new Date(),
-                    toAccountId: this.transferRequest.toAccountId,
-                    amount: this.transferRequest.amount
+                    toAccountId: toAccountId,
+                    amount: amount,
                 });
                 this.isLoading.set(false);
             }
@@ -101,13 +100,12 @@ export class TransferComponent implements OnInit {
     }
 
     resetForm(): void {
-        this.lastTransaction.set(null);
+        this.transferResponse.set(null);
         this.failedTransaction.set(null);
-        this.successMessage.set('');
-        this.errorMessage.set('');
-        this.transferRequest.amount = null!;
 
-        // generate new idempotencyKey
-        this.transferRequest.idempotencyKey = crypto.randomUUID();
+        // reset form
+        this.transferForm.controls['toAccountId'].setValue('');
+        this.transferForm.controls['amount'].setValue(null);
+        this.submitted.set(false);
     }
 }
